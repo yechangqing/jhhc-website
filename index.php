496158f1c5d650e50296b40a9c77f43cbe77edf8
yechangqing <yecq@vip.163.com>
2016-02-18 13:00:51 +0800

    项目初始化

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>南京景和华诚信息科技有限公司</title>
+    </head>
+    <body>
+        <?php
+            echo '网站建设中...';
+        ?>
+    </body>
+</html>
